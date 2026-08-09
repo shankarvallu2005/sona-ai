a383c78ddba8909374e298b2594ee3ea640a9f48
V. Shankar <shankarvallu2005@gmail.com>
2026-08-09 23:43:50 +0530

    Load configuration from JSON

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-def main():
-    print("====================================")
-    print("       Welcome to Project SONA      ")
-    print("====================================")
-    print("SONA is starting...")
-    print("System initialized successfully.")
+import json
 
+def load_config():
+    with open("config/config.json", "r") as file:
+        return json.load(file)
+
+def main():
+    config = load_config
+
+    print("================================")
+    print(f"Welcome to {config['assistant_name']}")
+    print("================================")
+    print(f"Version   : {config['version']}")
+    print(f"Developer : {config['developer']}")
+    print(f"Theme     : {config['theme']}")
+    print(f"Language  : {config['language']}")
+    print("SONA is starting...")
 
 if __name__ == "__main__":
-    main() 
+    main()
